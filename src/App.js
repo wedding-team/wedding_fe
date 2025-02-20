3fa2b85fca280e4b9d20ad78f4c179f29566a513
truonghoi1205 <phamtruonghoi.dmx@gmail.com>
2025-02-20 17:18:53 +0700

    update form login and form sign up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import {ToastContainer} from "react-toastify";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import FormLogin from "./Component/Auth/FormLogin";
+import '../src/index.css';
+import FormSignUp from "./Component/Auth/FormSignUp";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
+                <Route path="/login" element={<FormLogin/>}/>
+                <Route path="/sign-up" element={<FormSignUp/>}/>
             </Routes>
             <ToastContainer/>
         </BrowserRouter>
