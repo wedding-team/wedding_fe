0b571128d3d1b1cb29e286800a05f8e734055d0c
truonghoi1205 <phamtruonghoi.dmx@gmail.com>
2025-02-28 19:22:20 +0700

    update form and fix call api login and signup

--- src/Pages/Auth/SignUp.js
+++ src/Pages/Auth/SignUp.js
@@ -1,8 +1,9 @@
-import FormLogin from "../../Component/Auth/FormLogin";
+import { Toaster } from "sonner";
 import { useFormik } from "formik";
-import AuthApi from "../../Apis/AuthApi";
-import Helper from "../../Utils/Helper";
 import * as Yup from "yup";
+import AuthApi from "../../Apis/AuthApi";
+import FormSignUp from "../../Component/Auth/FormSignUp";
+import Helper from "../../Utils/Helper";
 
 const validationSchema = Yup.object({
   email: Yup.string()
@@ -11,33 +12,39 @@ const validationSchema = Yup.object({
   password: Yup.string()
     .min(6, "Mật khẩu ít nhất 6 ký tự")
     .required("Vui lòng nhập mật khẩu"),
+  password_confirmation: Yup.string()
+    .oneOf(
+      [Yup.ref("password"), null],
+      "Xác nhận mật khẩu phải khớp với mật khẩu"
+    )
+    .required("Vui lòng nhập xác nhận mật khẩu"),
 });
 
-function Login() {
+function SignUp() {
   const formik = useFormik({
     initialValues: {
+      name: "",
       email: "",
       password: "",
+      password_confirmation: "",
     },
     validationSchema,
     onSubmit: async (values, { setSubmitting }) => {
-      try {
-        const res = await AuthApi.login({
-          email: values.email,
-          password: values.password,
-        });
-        console.log(res.data);
-        Helper.toastSuccess("Đăng nhập thành công!");
-      } catch (error) {
-        console.log(error);
-        Helper.toastError("Đăng nhập thành công!");
-      } finally {
-        setSubmitting(false);
-      }
-    },
+        try {
+            const res = await AuthApi.signUp(values);
+            console.log(res);
+            Helper.toastSuccess("Đăng ký thành công!");
+        } catch (error) {
+            Helper.handleApiError(error);
+        } finally {
+            setSubmitting(false);
+        }
+    },    
   });
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100 px-4">
+      <Toaster position="bottom-right" />
       <div className="w-full max-w-md bg-white rounded-lg shadow-md p-8">
         <div className="flex flex-col items-center">
           <img
@@ -45,9 +52,9 @@ function Login() {
             alt="Logo"
             className="h-10"
           />
-          <h2 className="mt-4 text-2xl font-bold text-gray-900">ĐĂNG NHẬP</h2>
+          <h2 className="mt-4 text-2xl font-bold text-gray-900">ĐĂNG KÝ</h2>
         </div>
-        <FormLogin formik={formik} />
+        <FormSignUp formik={formik} />
         <div className="relative mt-6">
           <div className="absolute inset-0 flex items-center">
             <div className="w-full border-t border-gray-300"></div>
@@ -56,10 +63,11 @@ function Login() {
             <span className="bg-white px-2 text-gray-600">Hoặc</span>
           </div>
         </div>
+
         <p className="mt-6 text-center text-sm text-gray-600">
-          Bạn chưa có tài khoản?
-          <a href="/sign-up" className="text-rose-600 hover:underline ms-1">
-            Đăng ký
+          Bạn đã có tài khoản?
+          <a href="/login" className="text-rose-600 hover:underline ms-1">
+            Đăng nhập
           </a>
         </p>
       </div>
@@ -67,4 +75,4 @@ function Login() {
   );
 }
 
-export default Login;
+export default SignUp;
